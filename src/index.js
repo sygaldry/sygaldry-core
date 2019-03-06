10ec479ba9a785f73f824e5e49cd2847cbe153ff
Brice Rising <brice.rising@Brices-MacBook-Pro.local>
2019-03-05 20:10:39 -0500

    Add index.js that exports everything
    This index.js will export everything for the core module

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+// Inversify types
+import { TYPES } from './types';
+
+// Clients
+import { NpmClient } from './clients/npm_client';
+
+// Interfaces
+import { BuilderClient } from './clients/interfaces/builder_client'
+import { PublisherClient } from './clients/interfaces/publisher_client'
+
+export { TYPES, NpmClient, BuilderClient, PublisherClient }
