a8abf9f6674d79eb47fc87c99ad089f18160d88c
Brice Rising <rising.brice@gmail.com>
2019-11-03 14:29:08 -0500

    Start validator
    Created a GetValidStage that validates the source runes.yaml and gets the runes from that yaml. If the yaml is invalid, an appropriate error is returned.

--- stage/stage.go
+++ stage/stage.go
@@ -2,10 +2,11 @@ package stage
 
 import (
 	"github.com/sygaldry/sygaldry-core/runes"
+	"github.com/sygaldry/sygaldry-core/validator"
 )
 
 // GetStage will validate and generate a list
 // of runes a user needs for a targeted stage
 func GetStage(source string, stage string) ([]runes.Rune, error) {
-	return []runes.Rune{}, nil
+	return validator.GetValidStage(source, stage)
 }
---
+++ validator/validator.go
@@ -0,0 +1,12 @@
+package validator
+
+import (
+	"github.com/sygaldry/sygaldry-core/runes"
+)
+
+// GetValidStage validates a rune yaml file
+// either a path on the local filesystem or a url
+// is passed in
+func GetValidStage(source string, stage string) ([]runes.Rune, error) {
+	return []runes.Rune{}, nil
+}
