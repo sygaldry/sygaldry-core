05697329154566d9ada2a68e1d18d7b4fa9b3dc8
Brice Rising <rising.brice@gmail.com>
2019-11-03 14:16:40 -0500

    Implement GetStage stub for cli to develop against
    This will be the entrypoint for the cli

---
+++ stage/stage.go
@@ -0,0 +1,11 @@
+package stage
+
+import (
+	"github.com/sygaldry/sygaldry-core/runes"
+)
+
+// GetStage will validate and generate a list
+// of runes a user needs for a targeted stage
+func GetStage(source string, stage string) ([]runes.Rune, error) {
+	return []runes.Rune{}, nil
+}
