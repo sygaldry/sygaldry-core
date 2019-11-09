ef354b4972693c0e673b5dc1802dd51fbd4d555e
Brice Rising <rising.brice@gmail.com>
2019-11-09 15:43:10 -0500

    Implement validators
    This builds runes based on a runes.yaml file. It needs more tests and better error handling, but it works!

--- validator/validator.go
+++ validator/validator.go
@@ -1,8 +1,10 @@
 package validator
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"text/template"
 
 	"github.com/smallfish/simpleyaml"
 	"github.com/sygaldry/sygaldry-core/runes"
@@ -27,22 +29,123 @@ func GetValidStage(source string, stage string) ([]runes.Rune, error) {
 	if getRuneConfigsErr != nil {
 		return nil, getRuneConfigsErr
 	}
+	runeConfigsLen, getRuneConfigsLenErr := runeConfigs.GetArraySize()
 
-	runeConfigsArray, arrayErr := runeConfigs.Array()
-
-	if arrayErr != nil {
-		return nil, arrayErr
+	if getRuneConfigsLenErr != nil {
+		return nil, fmt.Errorf("Could not get count of runes for stage %s", stage)
 	}
 
-	for _, element := range runeConfigsArray {
+	validStageRunes := make([]runes.Rune, runeConfigsLen)
 
+	for index := 0; index < runeConfigsLen; index++ {
+		runeConfig := runeConfigs.GetIndex(index)
+		if runeConfig != nil {
+			if valid, _ := validateRuneConfig(runeConfig, runeDefinitions); !valid {
+				validRune, buildValidRuneErr := buildValidRune(runeConfig, runeDefinitions)
+				if buildValidRuneErr != nil {
+					return nil, errors.New("Could not build rune")
+				}
+				validStageRunes[index] = validRune
+			}
+		} else {
+			return nil, fmt.Errorf("Could not parse runes for stage %s", stage)
+		}
 	}
 
-	return []runes.Rune{}, nil
+	return validStageRunes, nil
+}
+
+func buildValidRune(runeConfig *simpleyaml.Yaml, runeDefinitions *simpleyaml.Yaml) (runes.Rune, error) {
+	runeConfigDefinitionName, getStringErr := runeConfig.Get("definition").String()
+	if getStringErr != nil {
+		return runes.Rune{}, errors.New("Could not read rune definition name")
+	}
+
+	runeDefinition := runeDefinitions.Get(runeConfigDefinitionName)
+
+	runeDefinitionImageTemplate, getImageTemplateErr := runeDefinition.Get("values").Get("Image").String()
+	if getImageTemplateErr != nil {
+		return runes.Rune{}, fmt.Errorf("Could not read Image value for rune definition %s", runeConfigDefinitionName)
+	}
+	runeDefinitionEnvTemplates, getEnvTemplateErr := util.ConvertYamlListToStringList(runeDefinition.Get("values").Get("Env"))
+	if getEnvTemplateErr != nil {
+		runeDefinitionEnvTemplates = []string{}
+	}
+	runeDefinitionVolumesTemplates, getVolumesTemplateErr := util.ConvertYamlListToStringList(runeDefinition.Get("values").Get("Volumes"))
+	if getVolumesTemplateErr != nil {
+		runeDefinitionVolumesTemplates = []string{}
+	}
+
+	runeImage, imageBuildStringForTemplateErr := buildStringsForTemplate(runeDefinitionImageTemplate, runeConfig)
+	if imageBuildStringForTemplateErr != nil {
+		return runes.Rune{}, imageBuildStringForTemplateErr
+	}
+	runeEnv, envBuildStringsForTemplatesErr := buildStringsForTemplates(runeDefinitionEnvTemplates, runeConfig)
+	if envBuildStringsForTemplatesErr != nil {
+		return runes.Rune{}, envBuildStringsForTemplatesErr
+	}
+	runeVolumes, volumesBuildStringsForTemplatesErr := buildStringsForTemplates(runeDefinitionVolumesTemplates, runeConfig)
+	if volumesBuildStringsForTemplatesErr != nil {
+		return runes.Rune{}, volumesBuildStringsForTemplatesErr
+	}
+
+	return runes.NewRune(runeImage, runeEnv, runeVolumes)
+}
+
+func buildStringsForTemplate(runeValueTemplate string, runeConfig *simpleyaml.Yaml) (string, error) {
+	runeConfigMap, getRuneConfigMapErr := runeConfig.Map()
+	if getRuneConfigMapErr != nil {
+		return "", errors.New("Could not make map out of runeConfig")
+	}
+	valueTemplate, newValueTemplateErr := template.New("value").Parse(runeValueTemplate)
+	if newValueTemplateErr != nil {
+		return "", fmt.Errorf("Cound not create go template for: %s", runeValueTemplate)
+	}
+	var valueBytes bytes.Buffer
+	valueTemplate.Execute(&valueBytes, runeConfigMap)
+	return valueBytes.String(), nil
+}
+
+func buildStringsForTemplates(runeValueTemplates []string, runeConfig *simpleyaml.Yaml) ([]string, error) {
+	runeConfigMap, getRuneConfigMapErr := runeConfig.Map()
+	if getRuneConfigMapErr != nil {
+		return nil, errors.New("Could not make map out of runeConfig")
+	}
+	numValues := len(runeValueTemplates)
+	valuesBytes := make([]bytes.Buffer, numValues)
+	valuesStrings := make([]string, numValues)
+	for index, runeValueTemplate := range runeValueTemplates {
+		valueTemplate, newTemplateErr := template.New(fmt.Sprintf("value-%d", index)).Parse(runeValueTemplate)
+		if newTemplateErr != nil {
+			return nil, fmt.Errorf("Cound not create go template for: %s", runeValueTemplate)
+		}
+		valueTemplate.Execute(&valuesBytes[index], runeConfigMap)
+		valuesStrings[index] = valuesBytes[index].String()
+	}
+	return valuesStrings, nil
 }
 
 func validateRuneConfig(runeConfig *simpleyaml.Yaml, runeDefinitions *simpleyaml.Yaml) (bool, error) {
+	runeConfigDefinitionName, getStringErr := runeConfig.Get("definition").String()
+	if getStringErr != nil {
+		return false, errors.New("Could not read rune definition name")
+	}
+	runeDefinition := runeDefinitions.Get(runeConfigDefinitionName)
+	runeConfigParams, getMapKeysErr := runeConfig.GetMapKeys()
+	if getMapKeysErr != nil {
+		return false, fmt.Errorf("could not read keys for %s rune", runeConfigDefinitionName)
+	}
 
+	return validateRuneConfigParams(runeConfigParams, runeDefinition)
+}
+
+func validateRuneConfigParams(runeConfigParams []string, runeDefinition *simpleyaml.Yaml) (bool, error) {
+	runeDefinitionParams := runeDefinition.Get("params")
+	runeDefinitionParamsList, getStringErr := util.ConvertYamlListToStringList(runeDefinitionParams)
+	if getStringErr != nil {
+		errors.New("could not convert rune definition params to list of strings")
+	}
+	return util.ListContainsListStrings(runeDefinitionParamsList, runeConfigParams), nil
 }
 
 func getRunesConfigsForStage(runeYaml *simpleyaml.Yaml, stage string) (*simpleyaml.Yaml, error) {
@@ -90,18 +193,6 @@ func getRuneDefinitions(runeYaml *simpleyaml.Yaml) (*simpleyaml.Yaml, error) {
 	}
 
 	return simpleyaml.NewYaml(definitionsYamlBytes)
-}
-
-func getRunesRuneDefinitions(runeYaml *simpleyaml.Yaml, stage string) (*simpleyaml.Yaml, error) {
-	stageRunes := runeYaml.Get("stages").Get(stage)
-
-	if numRunes, _ := stageRunes.GetArraySize(); numRunes == 0 {
-		return nil, fmt.Errorf(
-			"Could not find any Runes for stage %s",
-			stage,
-		)
-	}
-	return stageRunes, nil
 }
 
 func yamlSource(source string) (*simpleyaml.Yaml, error) {
--- validator/validator_test.go
+++ validator/validator_test.go
@@ -5,11 +5,93 @@ import (
 )
 
 // TestRuneRun tests rune.Run
-func TestGetValidStage(t *testing.T) {
-	stage := "build"
+func TestYamlSource(t *testing.T) {
 	runesYamlPath := "test_resources/test-runes.yaml"
-	_, getValidStageErr := GetValidStage(runesYamlPath, stage)
+	_, yamlSourceErr := yamlSource(runesYamlPath)
+	if yamlSourceErr != nil {
+		t.Error("tried to load test-runes.yaml, got", yamlSourceErr)
+	}
+}
+
+func TestGetRunesConfigsForStage(t *testing.T) {
+	runesYamlPath := "test_resources/test-runes.yaml"
+	stage := "build"
+	runesYaml, yamlSourceErr := yamlSource(runesYamlPath)
+	if yamlSourceErr != nil {
+		t.Error("tried to load test-runes.yaml, got", yamlSourceErr)
+	}
+	runesConfigs, getRunesConfigsForStageErr := getRunesConfigsForStage(runesYaml, stage)
+	if getRunesConfigsForStageErr != nil {
+		t.Errorf("tried to get runeConfigs for stage %s, got %s", stage, getRunesConfigsForStageErr)
+	}
+	runeConfigsArray, getYamlArrayErr := runesConfigs.Array()
+	if getYamlArrayErr != nil {
+		t.Errorf("tried to read runeConfigs for stage %s, got %s", stage, getYamlArrayErr)
+	}
+	if len(runeConfigsArray) != 3 {
+		t.Errorf("tried to read all three rune configs for stage %s, found %d", stage, len(runeConfigsArray))
+	}
+}
+
+func TestGetRuneDefinitions(t *testing.T) {
+	runesYamlPath := "test_resources/test-runes.yaml"
+	runesYaml, yamlSourceErr := yamlSource(runesYamlPath)
+	if yamlSourceErr != nil {
+		t.Error("tried to load test-runes.yaml, got", yamlSourceErr)
+	}
+	runeDefinitions, getRuneDefinitionsErr := getRuneDefinitions(runesYaml)
+	if getRuneDefinitionsErr != nil {
+		t.Error("tried to get runeDefinitions, got", getRuneDefinitionsErr)
+	}
+	_, getMapErr := runeDefinitions.Map()
+	if getMapErr != nil {
+		t.Error("tried to read runeDefinitions as map, got", getMapErr)
+	}
+	mavenRune := runeDefinitions.Get("MavenRune")
+	mavenRuneParams := mavenRune.Get("params")
+	mavenRuneParamsList, getArrayErr := mavenRuneParams.Array()
+	if getArrayErr != nil {
+		t.Error("tried to get MavenRune params, got", getArrayErr)
+	}
+	if len(mavenRuneParamsList) < 1 {
+		t.Errorf("tried to get MavenRune params, got %d params", len(mavenRuneParamsList))
+	}
+}
+
+func TestValidateRuneConfig(t *testing.T) {
+	runesYamlPath := "test_resources/test-runes.yaml"
+	stage := "build"
+	runesYaml, yamlSourceErr := yamlSource(runesYamlPath)
+	if yamlSourceErr != nil {
+		t.Error("tried to load test-runes.yaml, got", yamlSourceErr)
+	}
+	runesConfigs, getRunesConfigsForStageErr := getRunesConfigsForStage(runesYaml, stage)
+	if getRunesConfigsForStageErr != nil {
+		t.Errorf("tried to get runeConfigs for stage %s, got %s", stage, getRunesConfigsForStageErr)
+	}
+	runeDefinitions, getRuneDefinitionsErr := getRuneDefinitions(runesYaml)
+	if getRuneDefinitionsErr != nil {
+		t.Error("tried to get runeDefinitions, got", getRuneDefinitionsErr)
+	}
+	runeConfig := runesConfigs.GetIndex(0)
+	validRuneConfig, validateRuneConfigErr := validateRuneConfig(runeConfig, runeDefinitions)
+	if validateRuneConfigErr != nil {
+		t.Error("tried to validate runeConfig, got", validateRuneConfigErr)
+	}
+	if !validRuneConfig {
+		t.Error("tried to validate runeConfig, expected true, got", validRuneConfig)
+	}
+
+}
+
+func TestGetValidStage(t *testing.T) {
+	runesYamlPath := "test_resources/test-runes.yaml"
+	stage := "build"
+	stageRunes, getValidStageErr := GetValidStage(runesYamlPath, stage)
 	if getValidStageErr != nil {
-		t.Error("Tried to get stage from runes-test.yaml, got", getValidStageErr)
+		t.Errorf("tried to read runes for stage %s from %s, got %s", stage, runesYamlPath, getValidStageErr)
+	}
+	if len(stageRunes) != 3 {
+		t.Errorf("tried to read all three rune configs for stage %s, found %d", stage, len(stageRunes))
 	}
 }
